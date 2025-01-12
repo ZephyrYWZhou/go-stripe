e069a78e17db83d510159e5ae90a6a71b2ed4879
ZephyrYWZhou <91304878+ZephyrYWZhou@users.noreply.github.com>
2025-01-12 15:07:47 -0600

    terminal.page impl, air intro

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,5 +3,7 @@ package main
 import "net/http"
 
 func (app *application) VirtualTermianl(w http.ResponseWriter, r *http.Request) {
-	app.infoLog.Println("Hit the Handler")
+	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
+		app.errorLog.Println(err)
+	}
 }
